ef4c392e930fe9c25096e997107a7c19dd4e251a
Dilsher10 <Dilsher.dahri10@gmail.com>
2024-06-26 03:26:34 +0500

    changes

--- 27. Remove Element.js
+++ 27. Remove Element.js
@@ -1,3 +1,23 @@
+/*
+To solve this problem in JavaScript, you can use a two-pointer approach. One pointer will traverse the array, 
+and the other will track the position of elements that are not equal to val. Here's how you can implement this:
+
+Explanation
+
+Initialization: Start with k = 0 which will count the number of elements not equal to val.
+
+Iteration: Traverse through the array nums.
+
+Comparison: For each element, check if it is not equal to val.
+
+Placement: If it is not equal, place it at the current position of k and increment k.
+
+Result: After the loop, k will be the number of elements that are not equal to val, and the first k elements of
+nums will contain the elements that are not equal to val.
+This approach ensures that you are modifying the array in place and only using O(1) extra space, which is 
+efficient.
+*/
+
 /**
  * @param {number[]} nums
  * @param {number} val
@@ -9,8 +29,8 @@ var removeElement = function (nums, val) {
 
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== val) {
-            nums[k] = nums[i]
-            k++
+            nums[k] = nums[i];
+            k++;
         }
     }
 
--- 88.Merge_Sorted_Array.js
+++ 88.Merge_Sorted_Array.js
@@ -1,3 +1,16 @@
+// Explanation
+// p1 starts at the last element of the initial part of nums1 (m - 1).
+// p2 starts at the last element of nums2 (n - 1).
+// p starts at the last element of the combined array length (m + n - 1).
+// The for loop iterates from the end of the combined array nums1 towards the beginning.
+
+// If p2 is less than 0, it means all elements from nums2 have been merged, and the loop can be terminated.
+// Inside the loop, compare the elements at nums1[p1] and nums2[p2].
+// If nums1[p1] is greater, set nums1[i] to nums1[p1] and decrement p1.
+// Otherwise, set nums1[i] to nums2[p2] and decrement p2.
+
+
+
 /**
  * @param {number[]} nums1
  * @param {number} m
@@ -8,19 +21,23 @@
 var merge = function (nums1, m, nums2, n) {
     let p1 = m - 1;
     let p2 = n - 1;
+    let p = m + n - 1;
 
-    for (let i = m + n - 1; i >= 0; i--) {
+    for (let i = p; i >= 0; i--) {
 
-        if (p2 < 0) {
-            break
-        }
+        if (p2 < 0) break;
 
         if (p1 >= 0 && nums1[p1] > nums2[p2]) {
-            nums1[i] = nums1[p1]
-            p1--
+            nums1[i] = nums1[p1];
+            p1--;
         } else {
-            nums1[i] = num2[p2]
-            p2--
+            nums1[i] = nums2[p2];
+            p2--;
         }
     }
 };
+
+
+
+
+
